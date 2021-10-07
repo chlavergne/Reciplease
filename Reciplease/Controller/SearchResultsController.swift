b8c64d6fa28b7d78d10bc5006d4c224bd68ee882
Christophe Lavergne <chlavergne@outlook.fr>
2021-10-07 09:52:35 +0200

    custom cell  ok

--- Reciplease/Controller/SearchResultsController.swift
+++ Reciplease/Controller/SearchResultsController.swift
@@ -6,28 +6,40 @@
 //
 
 import UIKit
+import Foundation
 
-class SearchResultsController: UITableViewController {
+class SearchResultsController: UIViewController {
 
+    let recipeCellId = "RecipeTableViewCell"
+    @IBOutlet weak var recipesTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Register Cell
+        recipesTableView.register(UINib.init(nibName: recipeCellId, bundle: nil), forCellReuseIdentifier: recipeCellId)
+//        recipesTableView.rowHeight = UITableView.automaticDimension
+        recipesTableView.separatorColor = UIColor.clear
+        recipesTableView.reloadData()
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        recipesTableView.reloadData()
     }
 }
 
-extension SearchResultsController: UITableViewDataSource {
+extension SearchResultsController: UITableViewDataSource, UITableViewDelegate {
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return RecipeService.shared.recipes.count
+        return IngredientService.shared.ingredients.count
     }
-    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "IngredientCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: recipeCellId, for: indexPath) as! RecipeTableViewCell
+        cell.selectionStyle = .none
         let ingredient = IngredientService.shared.ingredients[indexPath.row]
-        cell.textLabel?.text = ingredient.name
+        cell.subtitle?.text = ingredient.name
         return cell
     }
 }
-
---
+++ Reciplease/Extensions/Cells/RecipeTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  RecipeTableViewCell.swift
+//  Reciplease
+//
+//  Created by Christophe Expleo on 06/10/2021.
+//
+
+import UIKit
+
+class RecipeTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var subtitle: UILabel!
+    @IBOutlet weak var calories: UILabel!
+    @IBOutlet weak var totalTime: UILabel!
+    @IBOutlet weak var recipeImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
